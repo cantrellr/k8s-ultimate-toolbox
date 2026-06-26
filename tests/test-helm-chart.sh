ec40c2f1ef8e1ac65941a767e55b063e944826a9
Ron Cantrell <ron@cantrelloffice.cloud>
2026-06-26 07:57:34 -0400

    test: assert SELinux tools appear in rendered chart

--- tests/test-helm-chart.sh
+++ tests/test-helm-chart.sh
@@ -5,5 +5,7 @@ helm lint ./chart
 helm template test-render ./chart >/tmp/k8s-ultimate-toolbox-default-render.yaml
 
 grep -q 'k8s-ultimate-toolbox' /tmp/k8s-ultimate-toolbox-default-render.yaml
+grep -q 'SELinux/audit' /tmp/k8s-ultimate-toolbox-default-render.yaml
+grep -q 'semanage' /tmp/k8s-ultimate-toolbox-default-render.yaml
 
 echo 'K8s Ultimate Toolbox Helm smoke test passed.'
